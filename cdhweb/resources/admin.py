a931a1aca89525fc2c7bbeeb27759689060549c2
rlskoeser <rebecca.s.koeser@emory.edu>
2016-12-15 18:16:05 -0500

    Basic file attachment model
    Still needs many-to-many references from resources that are allowed
    to have file attachments.

--- cdhweb/resources/admin.py
+++ cdhweb/resources/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from cdhweb.resources.models import UserResource, ResourceType
+from cdhweb.resources.models import ResourceType, Attachment
 
 
 admin.site.register(ResourceType)
+admin.site.register(Attachment)
--- cdhweb/resources/models.py
+++ cdhweb/resources/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+from mezzanine.core.fields import FileField
 
 
 class ResourceType(models.Model):
@@ -22,4 +26,33 @@ class UserResource(models.Model):
     resource_type = models.ForeignKey(ResourceType, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     URL = models.URLField()
+
+
+class Attachment(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, blank=True)
+    file = FileField('Document', blank=True)
+    url = models.URLField(blank=True)
+    # TODO: needs validation (in admin?) to ensure one and only one of
+    # file and url is set
+    PDF = 'PDF'
+    MSWORD = 'DOC'
+    VIDEO = 'VIDEO'
+    URL = 'URL'
+    type_choices = (
+        (PDF, 'PDF Document'),
+        (MSWORD, 'MS Word Document'),
+        (VIDEO, 'Video'),
+        (URL, 'URL'),
+    )
+    attachment_type = models.CharField(max_length=255, choices=type_choices)
+    # NOTE: possibly also validation on types? url/video for url,
+    # pdf/word for file?
+
+    # NOTE: could do a generic many-to-many with django-gm2m, but it seems
+    # like it may be cleaner and simpler to just add many-to-many
+    # relationships on the models where we want to allow attachments
+
+    def __str__(self):
+        return self.title
 
