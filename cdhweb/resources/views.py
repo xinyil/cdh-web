b702beca8e4da01d088f34eb58f28a0073c89b42
rlskoeser <rebecca.s.koeser@emory.edu>
2016-12-16 18:08:00 -0500

    Preliminary logic for displaying projects & events on home page

--- cdhweb/resources/views.py
+++ cdhweb/resources/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
+from cdhweb.events.models import Event
+from cdhweb.projects.models import Project
 
 def site_index(request):
     '''Site home page.'''
-    # TODO: get random projects, upcoming events, and
-    # highlighted/featured item for display
-    return render(request, template_name='site_index.html')
+    # TODO: highlighted/featured item or news
+
+    projects = Project.objects.published(for_user=request.user) \
+        .active().current()
+    # NOTE: could potentially also filter by type of grant ?
+    # TODO: shuffle and pick first 3
+    # (shouldn't be so many current, active projects that this is a problem)
+
+    # find the next three upcoming, published events
+    upcoming_events = Event.objects.published(for_user=request.user) \
+        .upcoming()[:3]
+
+    return render(request, 'site_index.html',
+        {'projects': projects, 'events': upcoming_events})
