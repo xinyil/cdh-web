3cf95bab7b822b2cc9b96e9ec6c91ea7fa0f9c1f
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2017-01-19 11:33:37 -0500

    Reverse search to look for matching users, add --noop flag for future

--- cdhweb/resources/management/commands/importold.py
+++ cdhweb/resources/management/commands/importold.py
@@ -3,6 +3,7 @@ import os
 
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
+from cdhweb.people import models as people_models
 
 # Get the user model for use later
 User = get_user_model()
@@ -30,17 +31,29 @@ class Command(BaseCommand):
 
 
     def add_arguments(self, parser):
-        parser.add_argument('json_file', nargs='+', type=str)
-
+        parser.add_argument('json_file',
+            help='Path to a JSON dump created using dumpdata',
+            type=str
+        )
+        parser.add_argument(
+            '--noop',
+            action='store_true',
+            help="Don't write to database, only log process"
+        )
     def handle(self, *args, **options):
         '''Return a list of user emails to test'''
         # TODO: Make this actually import things.
-        listing = parse_model('staffprofiles.staffer', load_db(options['json_file'][0]))
+        listing = parse_model('staffprofiles.staffer', load_db(options['json_file']))
+
+        if options['noop']:
+            self.stdout.write(self.style.NOTICE('Running in no-op mode'))
+
         self.stdout.write('Parsing staff profile emails and usernames')
         self.stdout.write('-----')
         for person in listing:
             email = person['fields']['email']
             try:
+                user = User.objects.get(email=email)
                 self.stdout.write(self.style.SUCCESS(email))
-            except AttributeError:
+            except:
                 self.stdout.write(self.style.NOTICE("No email found."))
