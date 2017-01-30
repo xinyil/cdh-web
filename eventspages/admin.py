415564cdda4afee9f3371560b60ea9e5d560e708
bwhicks <benjamin.w.hicks@gmail.com>
2017-01-30 11:41:02 -0500

    One interface for events and their publishable

--- eventspages/admin.py
+++ eventspages/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Event, EventPage, EventsLandingPage
 
+
+class EventPageInline(admin.StackedInline):
+    model = EventPage
+    max_num = 1
+    fields = ('status', 'publish_date', 'expiry_date')
+
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [
+        EventPageInline
+            ]
+
+
 # Register your models here.a
-admin.site.register(Event)
-admin.site.register(EventPage)
+admin.site.register(Event, EventAdmin)
 admin.site.register(EventsLandingPage)
--- eventspages/apps.py
+++ eventspages/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class EventspagesConfig(AppConfig):
     name = 'eventspages'
+    verbose_name = 'Events Config'
