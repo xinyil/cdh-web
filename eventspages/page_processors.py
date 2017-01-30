23a1445ed5b1363ae510bcb1b24f93700d75a511
bwhicks <benjamin.w.hicks@gmail.com>
2017-01-30 11:02:20 -0500

    Create slug and processing for Fall2016 archive

--- eventspages/page_processors.py
+++ eventspages/page_processors.py
@@ -1,10 +1,22 @@
 from .models import EventsLandingPage, EventPage
 from mezzanine.pages.page_processors import processor_for
 
+from django.utils.dateparse import parse_datetime
+
+
 @processor_for(EventsLandingPage)
 def pull_published_events(request, page):
-    published_events  = EventPage.objects.filter(status=2)
+    published_events = EventPage.objects.filter(status=2).filter(
+         event_data__event_start_time__gte='2017-01-01'
+    )
     return {'events': published_events}
-     
+
+
+@processor_for('events/fall-2016')
+def pull_fall_2016_events(request, page):
+    published_events = EventPage.objects.filter(status=2).filter(
+        event_data__event_start_time__lt='2017-01-01'
+    )
+    return {'events': published_events}
 
 
