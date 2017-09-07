7dd70623e9958b53e4e354481651c24230348797
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-09-07 18:05:34 -0400

    Add blog post archive views and templates

--- cdhweb/blog/urls.py
+++ cdhweb/blog/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    url('^$', views.BlogListView.as_view(), name='list'),
-   url('^(?P<year>\d{4})/$', views.BlogListView.as_view(), name='year-list'),
+    url('^$', views.BlogIndexView.as_view(), name='list'),
+    url('^(?P<year>\d{4})/$', views.BlogYearArchiveView.as_view(),
+        name='by-year'),
     url("^(?P<year>\d{4})/(?P<month>\d{2})/(?P<slug>[\w-]+)/$",
         views.BlogDetailView.as_view(), name='detail'),
 ]
--- cdhweb/blog/views.py
+++ cdhweb/blog/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView
+
 
 from .models import BlogPost
 
 
-class BlogListView(ListView):
+class BlogIndexView(ArchiveIndexView):
 
     # same list view can power complete list or filtered version
     # by year, month, whatever
     # - pass year/month/etc filters in context data for display
     #   in the template
     model = BlogPost
+    date_field = 'publish_date'
+
+    def get_queryset(self):
+        qs = BlogPost.objects.published() # TODO: published(for_user=self.request.user)
+        if self.kwargs.get('year', None):
+            qs = qs.filter(publish_date__year=self.kwargs['year'])
+        return qs
+
+
+class BlogYearArchiveView(YearArchiveView):
+    date_field = "publish_date"
+    make_object_list = True
 
     def get_queryset(self):
         qs = BlogPost.objects.published() # TODO: published(for_user=self.request.user)
